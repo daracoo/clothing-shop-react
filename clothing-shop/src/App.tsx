e928e0ebf8fa595a815717a72759580afd06a919
daracoo <dkuzmanoski21@gmail.com>
2024-08-13 13:59:05 +0200

    Changed Header

--- clothing-shop/src/App.tsx
+++ clothing-shop/src/App.tsx
@@ -1,17 +1,10 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header'
 
 function App() {
   return (
     <div className='App'>
-      <Router>
         <Header />
-        <Routes>
-          <Route path='/' />
-          <Route path='/cart'/>
-        </Routes>
-      </Router>
     </div>
   )
 }
--- clothing-shop/src/components/Header.tsx
+++ clothing-shop/src/components/Header.tsx
@@ -1,14 +1,10 @@
-import { Link } from "react-router-dom"
 import { ShoppingCart } from "phosphor-react"
 import '../components/header.css'
 
 export const Header = () => {
     return <div className="navbar">
         <div className="links">
-            <Link to=""></Link>
-            <Link to="/cart">
-                <ShoppingCart size={32} />
-            </Link>
+            <button><ShoppingCart size={32} /></button>
         </div>
     </div>
 }
